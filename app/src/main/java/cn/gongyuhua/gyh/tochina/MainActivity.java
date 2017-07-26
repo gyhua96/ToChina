995aeebe8daa498c7cb3f75ebf4c03217734ddc3
ruanyulin <ruanyulin2017@gmail.com>
2017-07-26 11:21:36 +0800

    persion

--- app/src/main/java/cn/gongyuhua/gyh/tochina/MainActivity.java
+++ app/src/main/java/cn/gongyuhua/gyh/tochina/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
